1c0c63dfdb21c6a80a1b35b5296d7dd2b61df35b
sjb56 <sjb56@CS31C017855.ad.susx.ac.uk>
2017-03-14 13:17:55 +0000

    Added base java files (classes with extends/abstract applied where applicable for all classes in the V3 version of the UML diagram)

--- TradingSimulation/src/tradingsimulation/HiTechCompany.java
+++ TradingSimulation/src/tradingsimulation/HiTechCompany.java
@@ -9,13 +9,6 @@ package tradingsimulation;
  *
  * @author sjb56
  */
-public class TradingSimulation {
-
-    /**
-     * @param args the command line arguments,
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class HiTechCompany extends Company {
     
 }
