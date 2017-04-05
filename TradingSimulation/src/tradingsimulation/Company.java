8312770dc93238370785b5d8dd81574ef1477494
JGoodHub <jagoodmail@gmail.com>
2017-04-05 11:45:30 +0100

    Added constructor, get and set methods for company variables

--- TradingSimulation/src/tradingsimulation/Company.java
+++ TradingSimulation/src/tradingsimulation/Company.java
@@ -9,6 +9,58 @@ package tradingsimulation;
  *
  * @author sjb56
  */
-public class Company {
+public abstract class Company {
+    private int numberOfShares;
+    private int sharePrice;
+    protected RiskLevels riskFactor;
+    
+    public enum RiskLevels {
+        High, Medium, Low
+    }
+    
+    public Company (int startingShares, int startingPrice) {
+        numberOfShares = startingShares;
+        sharePrice = startingPrice;
+    }   
+    
+    public void addShares (int amount) {
+        numberOfShares += amount;        
+    }
+    
+    public void subtractShares (int amount) {
+        numberOfShares -= amount;
+    }
+    
+    public int getNumberOfShares () {
+        return numberOfShares;
+    }
+    
+    public void changeSharePrice (int newPrice) {
+        sharePrice = newPrice;
+    }
+    
+    public int getSharePrice () {
+        return sharePrice;
+    }
+    
+    public boolean setRiskFactor (int newRiskLevel) {
+        switch (newRiskLevel) {
+            case 0:
+                riskFactor = RiskLevels.Low;
+                break;
+            case 1:
+                riskFactor = RiskLevels.Medium;
+                break;
+            case 2:
+                riskFactor = RiskLevels.High;
+                break;
+            default:
+                return false;
+
+        }
+        
+        return true;
+        
+    }   
     
 }
